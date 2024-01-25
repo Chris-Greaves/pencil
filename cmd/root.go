b9413d8a8677027e30be1cf5c77ae4977c21495e
Christopher Greaves <28542943+Chris-Greaves@users.noreply.github.com>
2024-01-25 00:43:32 +0000

    Add description and flags

--- cmd/root.go
+++ cmd/root.go
@@ -4,6 +4,7 @@ Copyright © 2024 Chris Greaves cjgreaves97@hotmail.co.uk
 package cmd
 
 import (
+	"log"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -12,16 +13,22 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "pencil",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "A Tool to fill in the blanks.",
+	Long: `This tool uses a series of flags and the Go Templating library to
+fill in secrets and values in files and paths.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+Point it to a file and it'll treat the file like a Go Template and execute it, 
+pulling in Environment Variables and any other secrets passed into the tool.
+For example: "pencil -f app/config.yml -v SECRET_KEY=something-secret"`,
+
+	Run: func(cmd *cobra.Command, args []string) {
+		fileFlag := cmd.Flag("file").Value
+		log.Printf("Files: %s", fileFlag)
+		folderFlag := cmd.Flag("folder").Value
+		log.Printf("Folders: %s", folderFlag)
+		variableFlag := cmd.Flag("variable").Value
+		log.Printf("Variables: %s", variableFlag)
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -42,5 +49,7 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.Flags().StringArrayP("file", "f", make([]string, 0), "Path to a file you want run through the Template Engine")
+	rootCmd.Flags().StringArrayP("folder", "F", make([]string, 0), "Path to a directory you want all files underneath run through the Template Engine")
+	rootCmd.Flags().StringArrayP("variable", "v", make([]string, 0), "A variable to be used in the Templates. e.g. SECRET_KEY=something-secret")
 }
